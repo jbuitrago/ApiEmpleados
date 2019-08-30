9c21317fb7c46652ffe8831c5c729c5d960202d8
Jorge Buitrago <buirago.jorge@gmail.com>
2019-08-30 11:46:38 -0300

    modificacion relaciones

--- functions.js
+++ functions.js
@@ -39,39 +39,56 @@ let getLimitPaginate =  function getLimitPaginate(req, params, total) {
 
 let getRelation =  function getRelation(req,table) {
 
+if(Array.isArray(req.query.rel)){
+    console.log(req.query.rel );
     if (typeof req.query.rel !== 'undefined') {
-        
- 
         join = req.query.rel.map((rel) => {
             var relation = rel.split(".");
-            var joinTable = "";
-            var joinTableN = "";
-            var joinAlias = "";
-            var joinOn = "";
-            var i = 0;
-            var join = "";
-              for (i = 0; i < relation.length; i++) {
-                joinTable = relation[i];
-                if(joinTable == "manager"){
-                    joinTable = "employee";
-                    joinAlias = relation[i];
-                    joinOn = joinAlias + ".id" + " = "+ joinTable + "." + joinAlias;
-                }else if(joinTable == "superdepartment"){
-                    joinTableN = "department";
-                    joinAlias = relation[i];
-                    joinOn = joinTableN+ ".id" + " = "+ joinTableN + "." + joinAlias;
-                }else{
-                    joinAlias =joinTable;
-                    joinOn = joinAlias + ".id" + " = " + table + "." + joinAlias;
-                    join = join + " join " + joinTable + " " + joinAlias  + " on " + joinOn;
-               }
-                 
-             }
+             join=getJoin(relation,table);
              return join;
         });
     
     }
 
+}else{
+    var rel = [req.query.rel];
+    join = rel.map((rel) => {
+        var relation = rel.split(".");
+         join=getJoin(relation,table);
+         return join;
+    });
+}
+
+
     return join;
+}
+
+//Get Join
+function getJoin(relation,table){
+
+    var joinTable = "";
+    var joinTableN = "";
+    var joinAlias = "";
+    var joinOn = "";
+    var i = 0;
+    var join = "";
+      for (i = 0; i < relation.length; i++) {
+        joinTable = relation[i];
+        if(joinTable == "manager"){
+            joinTable = "employee";
+            joinAlias = relation[i];
+            joinOn = joinAlias + ".id" + " = "+ joinTable + "." + joinAlias;
+        }else if(joinTable == "superdepartment"){
+            joinTableN = "department";
+            joinAlias = relation[i];
+            joinOn = joinTableN+ ".id" + " = "+ joinTableN + "." + joinAlias;
+        }else{
+            joinAlias =joinTable;
+            joinOn = joinAlias + ".id" + " = " + table + "." + joinAlias;
+            join = join + " join " + joinTable + " " + joinAlias  + " on " + joinOn;
+       }
+         
+     }
+     return join;
 }
 module.exports = {getLimitPaginate , getRelation }
